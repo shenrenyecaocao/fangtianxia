ca1ddd0e223f6011767f40fbf65988bbb5b2f17a
shenrenyecaocao <541885781@qq.com>
2019-01-24 23:17:51 +0800

    ss

--- fangtianxia/pipelines.py
+++ fangtianxia/pipelines.py
@@ -11,11 +11,23 @@ class FangtianxiaPipeline(object):
     def open_spider(self, spider):
         filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', "house.json")
         self.f = open(filepath, "w")
+        self.f.write("[\n")
 
     def process_item(self, item, spider):
-        print json.dumps(dict(item), encoding='UTF-8', ensure_ascii=False)
-        self.f.write(json.dumps(dict(item)) + "\n")
+        data = dict(item)
+        print "$$$$$$$$$$$$$$$$$$$4"
+        print data
+        print "$$$$$$$$$$$$$$$$$$$4"
+
+        house_name = data.get('house_name', '')
+        house_address = data.get('house_address', '')
+        property_name = data.get('property_name', '')
+        property_address = data.get('property_address', '')
+        property_tel = data.get('property_tel', '')
+        info = '{"house_name": "%s", "house_address": "%s", "property_name": "%s", "property_address": "%s", "property_tel": "%s"},\n' % (house_name, house_address, property_name, property_address, property_tel)
+        self.f.write(info + "\n")
         return item
 
     def close_spider(self, spider):
+        self.f.write("]\n")
         self.f.close()
--- fangtianxia/spiders/ftx.py
+++ fangtianxia/spiders/ftx.py
@@ -2,8 +2,7 @@
 import scrapy
 from scrapy.http import Request
 from fangtianxia.items import FangtianxiaItem
-from scrapy.selector import Selector
-
+import sys
 
 class FangTianXiaSpider(scrapy.Spider):
     name = "fangtianxia"
@@ -11,43 +10,26 @@ class FangTianXiaSpider(scrapy.Spider):
     start_urls = [
         "https://xian.esf.fang.com/housing/"
     ]
-
     def parse(self, response):
         url_list = response.xpath('//div[@class="houseList"]//a[@class="plotTit"]/@href').extract()
         url_list = map(lambda url: "https:{}xiangqing/".format(url), url_list)
-        print url_list
         for url in url_list[:1]:
             yield Request(url, callback=self.parse_info)
 
         next_url = response.xpath('//a[@id="PageControl1_hlk_next"]/@href').extract_first()
         if next_url is not None:
             next_url = "https://xian.esf.fang.com{}".format(next_url)
-            yield scrapy.Request(next_url, callback=self.parse)
+            yield Request(next_url, callback=self.parse)
 
     def parse_info(self, response):
+        reload(sys)
+        sys.setdefaultencoding('utf8')
         item = FangtianxiaItem()
         item['house_name'] = response.xpath('//div[@class="logoBox_sq"]//a[@class="tt"]/text()').extract_first()
-        sel = Selector(response)
-        _dd = sel.xpath('//div[@class="box"]/div[@class="inforwrap clearfix"]/dl[@class=" clearfix mr30"]/dd')
-        # pprint(_dd)
-        # 小区地址
-        # 物业公司
-        # 业办公地点
-        # 物业办公电话
+        _dd = response.xpath('//div[@class="box"]/div[@class="inforwrap clearfix"]/dl[@class=" clearfix mr30"]/dd')
         for d in _dd:
             k = d.xpath('strong/text()').extract_first()
             v = d.xpath('text()').extract_first()
-            try:
-                k = k.encode('gbk')
-            except Exception as e:
-                k = k.encode('utf-8')
-                print e
-            print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-            print k
-            print type(k)
-            print '小区地址'
-            print type('小区地址')
-            print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
             if '小区地址' in k:
                 item['house_address'] = v
             else:
